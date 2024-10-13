1f3cd010048a87ebd36e9fc5a31df9f5f4240393
Jaime Elcarte <jelcarte@veridas.com>
2024-10-13 12:15:19 +0200

    Warn if duration is lower than 1

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const isValidDuration = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -24,7 +26,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!isValidDuration && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {isValidDuration && <Results input={userInput} />}
     </>
   );
 }
